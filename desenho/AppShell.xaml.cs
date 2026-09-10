28c8c54e29cefec8438fdfd93c79b594a1119dcd
Camillo11 <camillyvblackswan7@gmail.com>
2026-09-10 09:47:05 -0300

    Adicionar arquivos de projeto.

---
+++ desenho/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace desenho
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ desenho/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "desenho")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "desenho.Pages")]
